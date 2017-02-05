b8f2a6a7ae0f94099b269b5593adaf621ce00a9f
jakeoung <jakeoung@gmail.com>
2017-02-05 22:57:17 +0900

    add assets

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# u-net_pytorch
-Implementation of U-Net architecture using Pytorch
+# Implementation of U-Net architecture using Pytorch.
+
+This repository aims to practice pytorch and implement U-net architecture by Ronneberger et al. [1].
+The used dataset is nerve ultrasound images and we aim to delineate nerve structures from them.
+
+```
+usage: main.py [-h] [--workers WORKERS] [--batchSize BATCHSIZE]
+               [--niter NITER] [--start_epoch START_EPOCH] [--lr LR] [--cuda]
+               [--resume PATH] [--useBN] [--output_name OUTPUT_NAME]
+               dataroot
+
+positional arguments:
+  dataroot              path to dataset of kaggle ultrasound nerve
+                        segmentation
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --workers WORKERS     number of data loading workers
+  --batchSize BATCHSIZE
+                        input batch size
+  --niter NITER         number of epochs to train for
+  --start_epoch START_EPOCH
+                        number of epoch to start
+  --lr LR               learning rate, default=0.0002
+  --cuda                enables cuda
+  --resume PATH         path to latest checkpoint (default: none)
+  --useBN               enalbes batch normalization
+  --output_name OUTPUT_NAME
+                        output checkpoint filename
+```
+
+## References
+
+- [1] U-net paper: http://lmb.informatik.uni-freiburg.de/people/ronneber/u-net/
+- [2] Keras code: https://www.kaggle.com/c/ultrasound-nerve-segmentation
+- [3] Dataset: https://www.kaggle.com/c/ultrasound-nerve-segmentation
+
+## To-do
+
+- Do more tests
+- Add dice loss (Current loss is just MSEL2 loss)
+- U-net 2.0
+
+## Notes
+
+- Batch normalization is more robust than original version.
+without BN: (epoch 100, loss 2.056613136175425e-05)
+with BN: (epoch 100, loss 9.57660446428635e-07)
+
+## Sample results
+
+To run our pre-trained model, commit:
+
+```python3 main.py data --resume checkpoint_BN.tar --niter 0 --useBN```
+
+![original](assets/ori_5.png)
+![ground truth](assets/gt_5.png)
+![prediction](assets/pred_5.png)
--- main.py
+++ main.py
@@ -26,14 +26,15 @@ parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, def
 parser.add_argument('--cuda'  , action='store_true', help='enables cuda')
 parser.add_argument('--resume', default='', type=str, metavar='PATH', help='path to latest checkpoint (default: none)')
 parser.add_argument('--useBN', action='store_true', help='enalbes batch normalization')
-parser.add_argument('--output_name', default='checkpoint.tar', type=str, help='output checkpoint filename')
+parser.add_argument('--output_name', default='checkpoint___.tar', type=str, help='output checkpoint filename')
 
 args = parser.parse_args()
 print(args)
 
 ############## dataset processing
 dataset = kaggle2016nerve(args.dataroot)
-train_loader = torch.utils.data.DataLoader(dataset, batch_size=args.batchSize, num_workers=args.workers)
+train_loader = torch.utils.data.DataLoader(dataset, batch_size=args.batchSize,
+                                           num_workers=args.workers, shuffle=True)
 
 ############## create model
 model = Net(args.useBN)
@@ -45,12 +46,15 @@ if args.cuda:
 if args.resume:
   if os.path.isfile(args.resume):
     print("=> loading checkpoint '{}'".format(args.resume))
-    checkpoint = torch.load(args.resume)
+
+    if args.cuda == False:
+      checkpoint = torch.load(args.resume, map_location={'cuda:0':'cpu'})
+
     args.start_epoch = checkpoint['epoch']
 
     model.load_state_dict(checkpoint['state_dict'])
-    print("=> loaded checkpoint (epoch {})"
-        .format(checkpoint['epoch']))
+    print("=> loaded checkpoint (epoch {}, loss {})"
+        .format(checkpoint['epoch'], checkpoint['loss']) )
   else:
     print("=> no checkpoint found at '{}'".format(args.resume))
 
@@ -61,8 +65,6 @@ def save_checkpoint(state, filename=args.output_name):
 ############## training
 optimizer = optim.Adagrad(model.parameters(), lr=args.lr)
 model.train()
-
-# print(model.state_dict())
 
 def train(epoch):
   """
@@ -107,17 +109,33 @@ for epoch in range(args.niter):
 
 
 ############ just check test (visualization)
+
+def showImg(img, binary=True, fName=''):
+  """
+  show image from given numpy image
+  """
+  img = img[0,0,:,:]
+
+  if binary:
+    img = img > 0.5
+
+  img = Image.fromarray(np.uint8(img*255), mode='L')
+
+  if fName:
+    img.save('assets/'+fName+'.png')
+  else:
+    img.show()
+
+
 model.eval()
 train_loader.batch_size=1
 
 for i, (x,y) in enumerate(train_loader):
-  if i >= 1:
+  if i >= 11:
     break
 
   y_pred = model(Variable(x))
+  showImg(x.numpy(), binary=False, fName='ori_'+str(i))
+  showImg(y_pred.data.numpy(), binary=False, fName='pred_'+str(i))
+  showImg(y.numpy(), fName='gt_'+str(i))
 
-y_pred_numpy = y_pred.data.numpy()
-pred_img = y_pred_numpy[0,0,:,:]
-pred_img = pred_img > 0.5
-im = Image.fromarray(np.uint8(pred_img*255))
-im.show()
